7a31306d6c79c0683185ccd2185d0831ce557e97
Minh Phan <minhphan156@gmail.com>
2018-10-14 16:03:01 -0700

    finished search

--- front_end/src/components/layout/Search.js
+++ front_end/src/components/layout/Search.js
@@ -10,62 +10,21 @@ class Search extends Component {
 
     if (query != null) {
       const itemsList = query.map(item => {
-        // return (
-        //   <div className="col-2" key={item._id}>
-        //     <div className="card bg-light text-dark">
-
-        //       <div className="card-header d-flex align-items-center justify-content-center h-100">
-        //         <img
-        //           src={item.image}
-        //           className="img-thumbnail"
-        //           alt="Responsive image"
-        //         />
-        //       </div>
-
-        //       <div className="card-body">
-        //         <p>
-        //           {item.name}
-        //           <br />
-        //           {item.price}
-        //         </p>
-        //       </div>
-
-        //     </div>
-        //   </div>
-        // );
-
         return (
-          <ProductBar
-            productKey={item._id}
-            productImage={item.image}
-            productName={item.name}
-            productPrice={item.price}
-            productCategory={item.category}
-            productDescription={item.description}
-          />
+          <div className="col-md-2">
+            <ProductCard
+              productKey={item._id}
+              productImage={item.image}
+              productName={item.name}
+              productPrice={item.price}
+            />
+          </div>
         );
       });
 
-      // return (
-      //   <div className="landing">
-      //     <div className="landing-inner text-dark">
-      //       <div className="container">
-      //         <div className="row">
-      //           <div className="col-md-12 text-center">
-      //             <div style={flexContainer}>{itemsList}</div>
-      //             <hr />
-      //           </div>
-      //         </div>
-      //       </div>
-      //     </div>
-      //   </div>
-      // );
-
       return (
         <div className="container">
-          <div className="row">
-            <div>{itemsList}</div>
-          </div>
+          <div className="row">{itemsList}</div>
         </div>
       );
     }
@@ -73,13 +32,6 @@ class Search extends Component {
     return <div />;
   }
 }
-
-const flexContainer = {
-  display: "flex",
-  flex: 1,
-  flexDirection: "row",
-  flexWrap: "wrap"
-};
 
 const mapStateToProps = state => ({
   query: state.query
--- front_end/src/components/product-tiles/ProductCard.js
+++ front_end/src/components/product-tiles/ProductCard.js
@@ -7,20 +7,23 @@ import React, { Component } from "react";
 class ProductCard extends Component {
   render() {
     return (
-      <div>
+      <div key={this.props.productKey}>
         <div className="container product-card border p-0">
           <img
-            src="/category-imgs/Snacks_c.png"
+            src={this.props.productImage}
             alt="test-img"
             className="product-card-img"
           />
           <div className="product-card-info">
             <div className="product-card-info-left border-right border-top text-truncate p-0 m-0">
-              <p className="product-card-info-name m-0">Product-Name</p>
+              <p className="product-card-info-name m-0">
+                {this.props.productName}
+              </p>
             </div>
             <div className="product-card-info-right border-left border-top text-truncate p-2 m-0">
               <button type="button" className="btn product-card-info-btn">
-                <i className="fas fa-cart-plus" /> $XX.XX
+                <i className="fas fa-cart-plus" /> {this.props.productPrice}{" "}
+                cents
               </button>
             </div>
           </div>
