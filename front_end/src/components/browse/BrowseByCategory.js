49f75ffd64ad12e66fdcb381ca4eb40f6a84c1e1
Minh Phan <minhphan156@gmail.com>
2018-10-14 13:24:37 -0700

    install winston

--- front_end/src/components/browse/BrowseByCategory.js
+++ front_end/src/components/browse/BrowseByCategory.js
@@ -12,53 +12,81 @@ class BrowseByCategory extends Component {
         <div className="row justify-content-md-center">
           <div className="col-sm-2 border category-tile shadow">
             <Link to="/categories">
-              <img src="category-imgs/Snacks_c.png" className="category-img" alt="Snacks.img"/>
+              <img
+                src="category-imgs/Snacks_c.png"
+                className="category-img"
+                alt="Snacks.img"
+              />
               <br />
-              <div class="category-text">Snacks</div>
+              <div className="category-text">Snacks</div>
             </Link>
           </div>
           <div className="col-sm-2 border category-tile shadow">
             <Link to="/categories">
-              <img src="category-imgs/Drinks_c.png" className="category-img" alt="Drinks.img"/>
+              <img
+                src="category-imgs/Drinks_c.png"
+                className="category-img"
+                alt="Drinks.img"
+              />
               <br />
-              <div class="category-text">Drinks</div>
+              <div className="category-text">Drinks</div>
             </Link>
           </div>
           <div className="col-sm-2 border category-tile shadow">
             <Link to="/categories">
-              <img src="category-imgs/Produce_c.png" className="category-img" alt="Produce.img"/>
+              <img
+                src="category-imgs/Produce_c.png"
+                className="category-img"
+                alt="Produce.img"
+              />
               <br />
-              <div class="category-text">Produce</div>
+              <div className="category-text">Produce</div>
             </Link>
           </div>
         </div>
         <div className="row justify-content-md-center">
           <div className="col-sm-2 border category-tile shadow">
             <Link to="/categories">
-              <img src="category-imgs/Bakery_c.png" className="category-img" alt="Bakery.img"/>
+              <img
+                src="category-imgs/Bakery_c.png"
+                className="category-img"
+                alt="Bakery.img"
+              />
               <br />
-              <div class="category-text">Bakery</div>
+              <div className="category-text">Bakery</div>
             </Link>
           </div>
           <div className="col-sm-2 border category-tile shadow">
             <Link to="/categories">
-              <img src="category-imgs/Dairy_c.png" className="category-img" alt="Dairy.img"/>
+              <img
+                src="category-imgs/Dairy_c.png"
+                className="category-img"
+                alt="Dairy.img"
+              />
               <br />
-              <div class="category-text">Dairy</div>
+              <div className="category-text">Dairy</div>
             </Link>
           </div>
           <div className="col-sm-2 border category-tile shadow">
             <Link to="/categories">
-              <img src="category-imgs/Meats_c.png" className="category-img" alt="Meats.img"/>
+              <img
+                src="category-imgs/Meats_c.png"
+                className="category-img"
+                alt="Meats.img"
+              />
               <br />
-              <div class="category-text">Meats</div>
+              <div className="category-text">Meats</div>
             </Link>
           </div>
           <div className="col-sm-2 border category-tile shadow">
             <Link to="/categories">
-              <img src="category-imgs/Alcohol_c.png" className="category-img" alt="Alcohol.img"/>
+              <img
+                src="category-imgs/Alcohol_c.png"
+                className="category-img"
+                alt="Alcohol.img"
+              />
               <br />
-              <div class="category-text">Alcohol</div>
+              <div className="category-text">Alcohol</div>
             </Link>
           </div>
         </div>
